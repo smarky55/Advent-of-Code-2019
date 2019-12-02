ac317134e204edcc2885524f30979fa8407a845d
Sam Kingdon <smarky55@gmail.com>
2019-12-02 21:31:07 +0000

    Day one

---
+++ 01/01.py
@@ -0,0 +1,21 @@
+import math
+
+
+def fuel(mass):
+    return math.floor(int(mass)/3) - 2
+
+def fuel2(mass):
+    m_fuel = fuel(mass)
+    if m_fuel > 8:
+        return m_fuel + fuel2(m_fuel)
+    else:
+        return m_fuel
+
+
+with open('01/input.txt') as inFile:
+    modules = inFile.readlines()
+    print(sum(map(fuel, modules))) # Part 1
+
+    print(fuel2(1969))
+    # Part 2
+    print(sum(map(fuel2, modules)))
